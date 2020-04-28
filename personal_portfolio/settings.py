16ab1887165e2ab4fd71159055b6d5db8c6d0217
vengad98 <vengad98@gmail.com>
2020-04-28 09:45:06 +0100

    new domain

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -128,4 +128,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 try:
     from .local_settings import *
 except ImportError:
-    prtin("Looks like no local file. You must be on production")
+    print("Looks like no local file. You must be on production")

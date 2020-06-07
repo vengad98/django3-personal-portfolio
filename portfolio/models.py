bea53aac0aa6efbf136e32bfd819b00d7f212739
vengad98 <vengad98@gmail.com>
2020-06-07 12:07:56 +0100

    local

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
+    backend = models.CharField(max_length=250)
+    frontend = models.CharField(max_length=250)
     image = models.ImageField(upload_to='portfolio/images/')
     url = models.URLField(blank=True)
 
